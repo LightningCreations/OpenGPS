4031ebf91ca14fa9f2fb7a636a9f5e8d243e05c2
Connor Horman <chorman64@gmail.com>
2018-11-22 00:44:44 -0500

    Fix things and add stuff

---
+++ include/Node.hpp
@@ -0,0 +1,56 @@
+/*
+ * Node.hpp
+ *
+ *  Created on: Nov 22, 2018
+ *      Author: chorm
+ */
+
+#ifndef __INCLUDE_NODE_HPP__2018_11_22_00_08_03
+#define __INCLUDE_NODE_HPP__2018_11_22_00_08_03
+
+#include <lclib-cxx/Vector.hpp>
+#include <lclib-cxx/UUID.hpp>
+#include <lclib-cxx/IOWrapper.hpp>
+#include <cstdint>
+#include <list>
+#include <optional>
+#include <unordered_map>
+#include <string>
+#include <string_view>
+#include <filesystem>
+
+struct Node{
+	const Node* parent;
+	UUID id;
+	std::string name;
+	Vec2<std::uint64_t> pos;
+	std::list<UUID> children;
+	friend DataOutputStream& operator<<(DataOutputStream&,const Node&);
+	friend DataInputStream& operator>>(DataInputStream&,Node&);
+};
+
+constexpr UUID GLOBE{0,0};
+
+const Node& getNodeById(UUID);
+void loadMap(const std::filesystem::path&);
+
+enum class Direction{
+	NORTH, SOUTH, EAST, WEST
+};
+
+struct Connection{
+	const Node* first;
+	const Node* second;
+	double weight;
+	Direction dir;
+	friend DataOutputStream& operator<<(DataOutputStream&,const Connection&);
+	friend DataInputStream& operator>>(DataInputStream&,Connection&);
+};
+
+struct Route{
+	const Node* origin;
+	const Node* target;
+	std::list<const Connection*> path;
+};
+
+#endif /* __INCLUDE_NODE_HPP__2018_11_22_00_08_03 */
---
+++ include/opengps.hpp
@@ -0,0 +1,27 @@
+/*
+ * opengps.hpp
+ *
+ *  Created on: Nov 21, 2018
+ *      Author: chorm
+ */
+
+#ifndef __INCLUDE_OPENGPS_HPP__2018_11_21_23_59_47
+#define __INCLUDE_OPENGPS_HPP__2018_11_21_23_59_47
+
+#include <lclib-cxx/Config.hpp>
+#include <cstdint>
+#include <lclib-cxx/Vector.hpp>
+
+struct GPSCoord{
+	uint8_t degrees;
+	uint8_t minutes;
+	double seconds;
+};
+
+Vec2<std::uint64_t> fromGPSCoords(Vec2<GPSCoord>);
+Vec2<GPSCoord> toGPSCoords(Vec2<std::int64_t>);
+
+
+
+
+#endif /* __INCLUDE_OPENGPS_HPP__2018_11_21_23_59_47 */
